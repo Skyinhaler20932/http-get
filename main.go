f958c62782cdd84d3ec61d53c415d223114822be
joe <youssef.aboubaker8@gmail.com>
2026-05-17 22:28:12 +0300

    modify the print function output, and removing reduntant return

--- main.go
+++ main.go
@@ -41,7 +41,7 @@ func main() {
 	_, err := url.ParseRequestURI(rawURL)
 	// parsedURL, err := url.ParsedRequestURI(rawURL)
 	if err != nil {
-		fmt.Println("\n Invalid URL %s \n", err)
+		fmt.Printf("\n Invalid URL %v \n", err)
 		fmt.Println("Disconecting...\n")
 		time.Sleep(5 * time.Second)
 		os.Exit(1)
@@ -69,21 +69,19 @@ func main() {
 		}
 		fmt.Println("Retryting...", i+1)
 	}
-	if err != nil {
+	if errr != nil {
 		fmt.Println("Request failed:\n", err)
 		// as this error is system err not user one
 		// log.Fatal ==> same as: Printf(err), followed by os.Exit(1)
 		log.Fatal(err)
-		return
 	}
 	defer resp.Body.Close()
 	body, err := io.ReadAll(resp.Body)
 	if err != nil{
 	log.Fatal(err)
-	return
 	}
-	fmt.Printf("The status code is: %d \n",resp.StatusCode, "Body: %s", string(body) )
-	fmt.Println("The URL is:", resp.Request.URL.String())
+	fmt.Printf("The URL is: %s \n", resp.Request.URL.String())
+	fmt.Printf("HTTP Status Code: %d\nBody: %s\n", resp.StatusCode, string(body))
 	// the body is stream, and might be bigger than memory
 	// it's stream, and we need to close that stream once we finish
 	// defer => make sure that this runs at the end of the function
